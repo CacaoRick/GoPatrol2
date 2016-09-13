816540cb411cc78fe165eb527b9324a6c1cd3f27
CacaoRick <cacaorick@gmail.com>
2016-09-13 17:02:11 +0800

    加入 Pokemon-list

--- app/setting-general.js
+++ app/setting-general.js
@@ -2,6 +2,10 @@ let admins = [];
 let channels = [];
 let regexp = /^[a-zA-Z@][a-zA-Z0-9_]{3,29}[a-zA-Z0-9]$/;
 $(() => {
+
+	// 產生 Pokemon List
+	createPokemonTbody();
+
 	// 加入新管理員 By Enter
 	$("input#admin-input").keypress(e => {
 		if (e.which == 13) {
@@ -82,4 +86,16 @@ function removeChannel(span) {
 	let index = channels.indexOf($(span).parent().attr("channelId"));
 	channels.splice(index, 1);
 	$(span).parent().remove();
+}
+
+function createPokemonTbody() {
+	for (var i = 1; i <= 151; i++) {
+		let id = `<div class="caption"><h4>#${i}</h4></div>`;
+		let img = `<img src="assets/pokemon-3d/${i}.png" style="width: 96px; height: 96px; object-fit: contain">`;
+		let imform = `<label class="btn btn-primary"><input id="${i}" class="hidden" type="checkbox" data-toggle="buttons">通知</label>`;
+		let sticker = `<label class="btn btn-info"><input id="${i}" class="hidden" type="checkbox" data-toggle="buttons">帖圖</label>`;
+		let checkProperty = `<label class="btn btn-warning"><input id="${i}" class="hidden" type="checkbox" data-toggle="buttons">屬性</label>`;
+		let ivFilter = `<p class="text-center"><label class="control-label">IV篩選≧<input id="${i}" class="text-center" type="number" min="0" max="100" value="0"></label></p>`;
+		$("#pokemon_list").append(`<div #="${i}" class=" thumbnail col-md-2">${id}${img}<br>${imform}${sticker}${checkProperty}${ivFilter}</div>`);	
+	}
 }
