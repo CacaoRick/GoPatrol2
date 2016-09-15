91f6f0fbf5b063bb34c127a3445732dad98bca30
CacaoRick <cacaorick@gmail.com>
2016-09-15 17:05:40 +0800

    將預設設定改為 js 檔

---
+++ app/default-general.js
@@ -0,0 +1,19 @@
+
+module.exports = {
+	configVersion: 1,
+	googleMapsAPIKey: "",
+	telegramBotToken: "",
+	sendVenue: false,
+	showDistance: false,
+	useDistanceLocation: false,
+	distanceLocation: {
+		latitude: 25.05,
+		longitude: 121.55
+	},
+	enableCommand: false,
+	admins: [],
+	channels: [],
+	requestDelay: 5000,
+	pokemon_name: 0,
+	pokemon_list: []
+}
---
+++ app/default-location.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+	centerMode: true,
+	steps: 2,
+	centerLocation: {
+		latitude: 25.05,
+		longitude: 121.55
+	},
+	locations: []	
+}
