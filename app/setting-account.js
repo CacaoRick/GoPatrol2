2105353e8272f97d0a580e7cedb2a690a1ef5f2f
CacaoRick <cacaorick@gmail.com>
2016-09-16 20:59:51 +0800

    完成修改和刪除功能

--- app/setting-account.js
+++ app/setting-account.js
@@ -12,10 +12,25 @@ $(() => {
 			$("input#password").val("");
 		}
 		if (action == "編輯") {
+			let accounts;
+			if (button.data("type") == "patrol") {
+				accounts = patrols;
+			}
+			if (button.data("type") == "ivChecker") {
+				accounts = ivCheckers;
+			}
+			
+			// 取出編輯按鈕帶有的 username
+			let username = button.data("username");
+			// 找出他在 accounts 中的 index
+			let index = indexOfUsername(accounts, username);	
+
 			// 將要編輯的資料從 patrols 或 ivCheckers 中抓出來顯示在欄位中
-			$("input#username").val();
-			$("input#password").val();
-			$("select#provider").val();
+			$("input#username").val(accounts[index].username);
+			$("input#password").val(accounts[index].password);
+			$("select#provider").val(accounts[index].provider);
+			// 在 saveAccount 按鈕存入 index
+			modal.find("button#saveAccount").data("index", index);
 		}
 		
 		modal.find(".modal-title").text(action + "帳號");
@@ -25,15 +40,16 @@ $(() => {
 
 	$("button#saveAccount").click(event => {
 		let button = $(event.target);
+		let type = button.data("type"); 
 		let username = $("input#username").val();
 		let password = $("input#password").val();
 		let provider = $("select#provider").val();
 
 		let accounts;
-		if (button.data("type") == "patrol") {
+		if (type == "patrol") {
 			accounts = patrols;
 		}
-		if (button.data("type") == "ivChecker") {
+		if (type == "ivChecker") {
 			accounts = ivCheckers;
 		}
 
@@ -41,8 +57,6 @@ $(() => {
 			console.log("帳號未輸入");
 		} else if (password == "") {	
 			console.log("密碼未輸入");
-		} else if (indexOfUsername(accounts, username) >= 0) {
-			console.log("帳號重複");
 		} else {
 			let account = {
 				username: username,
@@ -51,21 +65,40 @@ $(() => {
 			}
 
 			if (button.data("action") == "新增") {
-				addAccount(button.data("type"), account);
+				if (indexOfUsername(accounts, username) >= 0) {
+					console.log("帳號重複");
+				} else {
+					addAccount(type, account);
+				}
 			}
 			if (button.data("action") == "編輯") {
-
+				let index = button.data("index");
+				accounts[index] = account;
+				loadAccount(type, accounts);
 			}
 			$("#account-editor").modal("hide");
 		}
 	});
 });
 
+function deleteAccount(type, username) {
+	let accounts;
+	if (type == "patrol") {
+		accounts = patrols;
+	}
+	if (type == "ivChecker") {
+		accounts = ivCheckers;
+	}
+	let index = indexOfUsername(accounts, username);
+	accounts.splice(index, 1);
+	loadAccount(type, accounts);
+}
+
 // 找出 username 在 accounts 中的 index，回傳值跟 indexOf 一樣
 function indexOfUsername(accounts, username) {
 	return accounts.map(account => {
 		return account.username;
-	}).indexOf(username);
+	}).indexOf(username.toString());
 }
 
 function addAccount(type, account) {
@@ -73,7 +106,7 @@ function addAccount(type, account) {
 	if (type == "patrol") {
 		patrols.push(account);
 	} else {
-		ivChecker.push(account);
+		ivCheckers.push(account);
 	}
 }
 
@@ -96,6 +129,6 @@ function loadAccount(type, accounts) {
 
 function buildActionButton(type, username) {
 	let editButton = `<button type="button" class="btn btn-primary btn-xs" data-toggle="modal" data-target="#account-editor" data-action="編輯" data-type="${type}" data-username="${username}"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span> 編輯</button>`;
-	let deleteButton = `<button type="button" class="btn btn-danger btn-xs" data-toggle="modal" data-target="#account-editor" data-action="刪除" data-type="${type}" data-username="${username}"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span> 刪除</button>`;
+	let deleteButton = `<button type="button" class="btn btn-danger btn-xs" onclick="deleteAccount('${type}', '${username}')"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span> 刪除</button>`;
 	return editButton + " " + deleteButton;
 }
