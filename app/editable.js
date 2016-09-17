fc38746ee62bf7e9cd7793932b9bfb703a066d14
CacaoRick <cacaorick@gmail.com>
2016-09-17 21:35:26 +0800

    加入 editable 相關的東西

---
+++ app/editable.js
@@ -0,0 +1,21 @@
+function bindEditable() {
+    $(".editable").off("click");
+    $(".editable-input").off("focusout");
+
+	$(".editable").click(event => {
+		let editable = $(event.target);
+		let input = editable.parent().find(".editable-input");
+		input.val(editable.text());
+		editable.hide();
+		input.show();
+		input.focus();
+	});
+	
+	$(".editable-input").focusout(event => {
+		let input = $(event.target);
+		let editable = input.parent().find(".editable");
+		editable.text(input.val());
+		input.hide();
+		editable.show();		
+	});
+}
