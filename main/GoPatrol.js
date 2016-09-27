3cf171fe62a83138d167eedf52e51c16d386cb5d
CacaoRick <cacaorick@gmail.com>
2016-09-27 23:37:45 +0800

    還是讓 GoPatrol 繼承 EventEmitter，這樣可以用 this.emit 來控制所有 task。task.setConfig 要比 task.setAccount 早呼叫

--- main/GoPatrol.js
+++ main/GoPatrol.js
@@ -1,55 +1,58 @@
 "use strict";
 const _ = require("lodash");
 const EventEmitter = require("events");
-const Promise = require("bluebird");
 const Task = require("./Task.js");
 
+class GoPatrol extends EventEmitter {
+	constructor() {
+		super();
+		this.isRunning = false;
+		this.tasks = [];
+		this.bindEvent();
+	}
 
-class GoPatrol {
-    constructor() {
-        super();
-        this.event = new EventEmitter();
-        this.isRunning = false;
-        this.tasks = [];
-    }
+	setConfig(config) {
+		this.config = config;
 
-    setConfig() {
-        this.config = config;
-    }
+		// 分配任務
+		this.config.location.forEach(location => {
+			let task = new Task(location, this);
+			this.tasks.push(task);
 
-    start() {
-        // 分配任務
-        config.location.forEach(location => {
-            let task = new Task(location, this.event);
-            tasks.push(task);
+			// 找出特定任務名稱的帳號
+			let accounts = _.filter(config.account, account => {
+				return account.task == location.name;
+			});
 
-            // 找出特定任務名稱的帳號
-            let accounts = _.filter(config.account, account => {
-                return account.task == location.name;
-            });
+			task.setConfig(this.config);
+			task.setAccount(accounts);
+		});
+	}
 
-            task.setAccount(accounts);
-            task.start();
-        });
-    }
+	start() {
+		if (!this.isRunning) {
+			this.isRunning = true;
+			console.log("emit start");
+			this.emit("start");
+		}
+	}
 
-    bindEvent() {
-        this.event.on("config", () => {
-            gopatrol.setConfig(config);
-        });
+	stop() {
+		if (this.isRunning) {
+			this.isRunning = false;
+			this.emit("stop");
+		}
+	}
 
-        this.event.on("start", () => {
-            gopatrol.start();
-        });
+	bindEvent() {
+		this.on("scannedPoint", point => {
+			this.emit("stop");
+		});
 
-        this.event.on("stop", () => {
-            gopatrol.stop();
-        });
-
-        this.event.on("newPokemon", pokemon => {
-            console.log(pokemon);
-        });
-    }
+		this.on("newPokemon", pokemon => {
+			console.log(pokemon);
+		});
+	}
 }
 
 module.exports = GoPatrol;
