963fad9e154f21174127d441654c332f953c9200
CacaoRick <cacaorick@gmail.com>
2016-09-27 23:38:42 +0800

    task 繼承 EventEmitter，用Promise來讓所有 patrol 開始執行。

--- main/Task.js
+++ main/Task.js
@@ -1,14 +1,22 @@
 "use strict";
 const _ = require("lodash");
+const EventEmitter = require("events");
 const Promise = require("bluebird");
 const Patrol = require("./Patrol.js");
 const hexGrid = require("../util/hex-grid.js");
 
-class Task {
-	constructor(location, event) {
+class Task extends EventEmitter {
+	constructor(location, gopatrol) {
+		super();
+		this.gopatrol = gopatrol;
+		this.isRunning = false;
 		this.patrols = [];
 		this.location = location;
-		this.event = event;
+		this.bindEvent();
+	}
+
+	setConfig(config) {
+		this.config = config;
 	}
 
 	setAccount(accounts) {
@@ -16,7 +24,7 @@ class Task {
 		let pointsPerWorker = Math.floor(patrolPoints.length / accounts.length);
 		let lastPoints = patrolPoints.length % accounts.length;
 
-		accounts.forEach((account) => {
+		accounts.forEach(account => {
 			let getPoints = pointsPerWorker;
 			if (lastPoints > 0) {
 				getPoints++;
@@ -25,19 +33,51 @@ class Task {
 
 			let points = _.take(patrolPoints, getPoints);
 			patrolPoints = _.drop(patrolPoints, getPoints);
-
-			let patrol = new Patrol(account, this.event);
+			let patrol = new Patrol(this.config.general, account, this);
 			patrol.setPoints(points);
 			this.patrols.push(patrol);
 		});
 	}
 
 	start() {
-		this.patrols.forEach(patrol => {
-			patrol.start();
+		if (!this.isRunning) {
+			this.isRunning = true;
+			let runs = [];
+			this.patrols.forEach(patrol => {
+				runs.push(patrol.run());
+			});
+
+			new Promise.all(runs)
+				.then(status => {
+					console.log(status);
+				});
+		}
+	}
+
+	stop() {
+		if (this.isRunning) {
+			this.isRunning = false;
+
+		}
+	}
+
+	bindEvent() {
+		this.gopatrol.on("start", () => {
+			this.start();
+		});
+
+		this.gopatrol.on("stop", () => {
+			this.stop();
+		});
+
+		this.on("scannedPoint", (point, cell) => {
+
+		});
+
+		this.on("scannedPoint", (account, error) => {
+
 		});
 	}
-	
 }
 
 module.exports = Task;
