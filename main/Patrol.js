bf1b0c5f7f269ffd10dcd98811ccdbb5a39d27f2
CacaoRick <cacaorick@gmail.com>
2016-09-27 23:39:17 +0800

    Promise 大致上做好了，現在差使用 pogobuf 取得資料以後的整理

--- main/Patrol.js
+++ main/Patrol.js
@@ -1,19 +1,84 @@
 "use strict";
 const pogobuf = require("pogobuf");
+const POGOProtos = require('node-pogo-protos');
 const Promise = require("bluebird");
 
+// this.task.emit("scannedPoint", point, cell);
+// this.task.emit("accountError", account, error);
+
 class Patrol {
-	constructor(account, event) {
+	constructor(config, account, task) {
+		this.config = config;
 		this.account = account;
-		this.event = event;
+		this.task = task;
+		this.client = new pogobuf.Client();
+		this.login = null;
+		if (this.account.provider == "ptc") {
+			this.login = new pogobuf.PTCLogin();
+		}
+		if (this.account.provider == "google") {
+			this.login = new pogobuf.GoogleLogin();
+		}
 	}
 
 	setPoints(points) {
 		this.points = points;
 	}
 
-	start() {
-		
+	run() {
+		return new Promise((resolve, reject) => {
+			if (this.login != null) {
+				this.login.login(this.account.username, this.account.password)
+					.catch(error => {
+						return `username: ${this.account.username}，provider: ${this.account.provider} 登入失敗，error: ${error.message}`;
+					})
+					.then(token => {
+						this.client.setAuthInfo(this.account.provider, token);
+						this.client.setPosition(this.points[0].latitude, this.points[0].longitude);
+						return this.client.init();
+					})
+					.catch(error => {
+						return `username: ${this.account.username}，Client.init 失敗，error: ${error.message}`;
+					})
+					.then(() => {
+						// 把每一點巡邏跑完
+						new Promise.reduce(this.points, (_, point) => {
+							return this.scanPoint(point, this.config.requestDelay);
+						}, null).then(result => {
+							resolve(this.account.username);
+						});
+					})
+					.catch(error => {
+						return `username: ${this.account.username}，error: ${error.message}`;
+					});
+			} else {
+				reject(Error(`username: ${this.account.username}，provider: ${this.account.provider} 設定錯誤`));
+			}
+		}).catch(error => {
+			return error.message;
+		});
+	}
+
+	scanPoint(point, delay) {
+		return new Promise((resolve, reject) => {
+			setTimeout(() => {
+				console.log("===============", point, "===============");
+				let cellIDs = pogobuf.Utils.getCellIDs(point.latitude, point.longitude);
+				return Promise.resolve(this.client.getMapObjects(cellIDs, Array(cellIDs.length).fill(0))).then(mapObjects => {
+					return mapObjects.map_cells;
+				}).each(cell => {
+					console.log('Cell ' + cell.s2_cell_id.toString());
+					console.log('Has ' + cell.catchable_pokemons.length + ' catchable Pokemon');
+					return Promise.resolve(cell.catchable_pokemons).each(catchablePokemon => {
+						console.log(' - A ' + pogobuf.Utils.getEnumKeyByValue(POGOProtos.Enums.PokemonId,
+							catchablePokemon.pokemon_id) + ' is asking you to catch it.');
+					});
+				});
+			}, delay);
+		}).catch(error => {
+			console.log(error);
+			return error;
+		});
 	}
 }
 
